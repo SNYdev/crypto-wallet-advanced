93eca22a6de495352b528aebe1e80c6d857a8e42
SNYdev <yanis.sahnoune@supinternet.fr>
2017-12-10 22:13:03 +0100

    add front dep

--- index.php
+++ index.php
@@ -11,13 +11,31 @@ require_once __DIR__."/core/core.php";
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Crypto Wallet Advanced</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
-<div class="container" style="height: 100px; background-color: red;">
-
+<div class="container-flex">
+    <div class="content-flex-auto content-flex-width-3" style="background-color: #3498db;">
+        <p>
+            TITRE 1
+        </p>
+    </div>
+    <div class="content-flex-auto content-flex-width-3" style="background-color: #2ecc71;">
+        <p>
+            TITRE 2
+        </p>
+    </div>
+    <div class="content-flex-auto content-flex-width-3" style="background-color: #e74c3c;">
+        <p>
+            TITRE 3
+        </p>
+    </div>
 </div>
+
+<footer>
+</footer>
 
 </body>
 </html>
