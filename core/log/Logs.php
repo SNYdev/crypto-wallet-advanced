be20b8988e7afc448bb7a69a1785cb3565fd0b85
SNYdev <yanis.sahnoune@supinternet.fr>
2017-12-26 12:47:16 +0100

    add logs

--- core/log/Logs.php
+++ core/log/Logs.php
@@ -1,8 +1,31 @@
 <?php
 
-namespace CryptoWalletAdvanced\Log;
+namespace CryptoWalletAdvancedLogs;
 
 class Logs
 {
+    static function accessLog($access, $isActive)
+    {
+        if (isActivgite){
+            $openFile = fopen(str_replace("Logs.php", "", __FILE__)."access.log", "a");
+            fwrite($openFile, __FILE__." : ".$access);
+            fclose($openFile);
+        } else {
+            var_dump("Access log is not active");
+        }
+    }
 
+    static function errorLog($error)
+    {
+        try{
+            $error;
+        } catch (\Exception $oException){
+            $openFile = fopen("error.log","r+");
+            fwrite($openFile, " : ". $oException->getMessage());
+            fwrite($openFile, " : ". $oException->getCode());
+            fwrite($openFile, " : ". $oException->getLine());
+            fwrite($openFile, " : ". $oException->getTrace());
+            fclose($openFile);
+        }
+    }
 }
